aa97a8b553367cfb290c3d82017b01d4feab355b
Mateus77//Henrique77// <henriquemateus.prof@gmail.com>
2023-11-29 22:16:52 +0000

    Update - Frontend em Páginas

--- src/views/dashboard/backoffice/Ferias.js
+++ src/views/dashboard/backoffice/Ferias.js
@@ -1,44 +1,17 @@
 import React from "react";
-import { Row, Col, Table } from "react-bootstrap";
+import { Row, Col, Table, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Card from "../../../components/Card";
 
-const Feriasmap = [
-  {
-    name: "Aprovado",
-    position: "Vera Alves",
-    office: "10/10/23 a 13/10/23",
-    age: "4 dias",
-    startdate: "2023",
-    salary: "✅ ➖ ❌",
-  },
-  {
-    name: "Aprovado",
-    position: "Manuel Pinto",
-    office: "23/11/23 a 24/11/23",
-    age: "2 dias",
-    startdate: "2023",
-    salary: "✅ ➖ ❌",
-  },
-  {
-    name: "Recusado",
-    position: "José Santos",
-    office: "26/12/23 a 29/10/23",
-    age: "4 dias",
-    startdate: "2023",
-    salary: "✅ ➖ ❌",
-  },
-  {
-    name: "Pendente",
-    position: "Beatriz Santos",
-    office: "15/01/24 a 19/01/24",
-    age: "5 dias",
-    startdate: "2024",
-    salary: "✅ ➖ ❌",
-  },
-];
-
 const Ferias = () => {
+  const handleSimClick = () => {
+    // Lógica para lidar com o clique no botão "Sim"
+  };
+
+  const handleNaoClick = () => {
+    // Lógica para lidar com o clique no botão "Não"
+  };
+
   return (
     <>
       <Row>
@@ -46,161 +19,49 @@ const Ferias = () => {
           <Card>
             <Card.Header className="d-flex justify-content-between">
               <div className="header-title">
-                <h4 className="card-title">Férias submetidas</h4>
+                <h4 className="card-title">Férias</h4>
               </div>
             </Card.Header>
-            <Card.Body>
-              <p>
-              <code>🔻</code> novas submissões submetidas{" "}
-              </p>
-              <div className="table-responsive border-bottom my-3">
-                <Table
-                  responsive
-                  striped
-                  id="datatable"
-                  className=""
-                  data-toggle="data-table"
-                >
+            <Card.Body className="p-0">
+              <div className="table-responsive mt-4">
+                <Table striped id="basic-table" className="mb-0" role="grid">
                   <thead>
                     <tr>
-                      <th>Estado</th>
-                      <th>Utilizador</th>
-                      <th>Datas</th>
-                      <th>Dias</th>
-                      <th>Ano</th>
-                      <th>Aprovação</th>
+                      <th className="text-center">Utilizador</th>
+                      <th className="text-center">Ano</th>
+                      <th className="text-center">Mês</th>
+                      <th className="text-center">Data de submissão</th>
+                      <th className="text-center">Total Horas</th>
+                      <th className="text-center">Aprovação</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {Feriasmap.map((item) => (
-                      <tr key={item.age}>
-                        <td>{item.name}</td>
-                        <td>{item.position}</td>
-                        <td>{item.office}</td>
-                        <td>{item.age}</td>
-                        <td>{item.startdate}</td>
-                        <td>{item.salary}</td>
-                      </tr>
-                    ))}
+                    <tr>
+                      <td className="text-center">
+                        <h6>Vera Alves</h6>
+                      </td>
+                      <td className="text-center">
+                        <div>
+                          <div>
+                            2023
+                          </div>
+                        </div>
+                      </td>
+                      <td className="text-center">Janeiro</td>
+                      <td className="text-center">
+                        <div>31/01/2023</div>
+                      </td>
+                      <td className="text-center">160</td>
+                      <td>
+                        <div className="d-flex align-items-center justify-content-center mb-2">
+                          <Button variant="success" onClick={handleSimClick}>                                <svg class="icon-32" width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M16.3345 2.75024H7.66549C4.64449 2.75024 2.75049 4.88924 2.75049 7.91624V16.0842C2.75049 19.1112 4.63549 21.2502 7.66549 21.2502H16.3335C19.3645 21.2502 21.2505 19.1112 21.2505 16.0842V7.91624C21.2505 4.88924 19.3645 2.75024 16.3345 2.75024Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>                                    <path d="M8.43994 12.0002L10.8139 14.3732L15.5599 9.6272" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>                                </svg>                            </Button>
+                          <td></td><td></td>
+                          <Button variant="danger" onClick={handleNaoClick}>                                <svg class="icon-32" width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">                                    <path d="M14.3955 9.59497L9.60352 14.387" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>                                    <path d="M14.3971 14.3898L9.60107 9.59277" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M16.3345 2.75024H7.66549C4.64449 2.75024 2.75049 4.88924 2.75049 7.91624V16.0842C2.75049 19.1112 4.63549 21.2502 7.66549 21.2502H16.3335C19.3645 21.2502 21.2505 19.1112 21.2505 16.0842V7.91624C21.2505 4.88924 19.3645 2.75024 16.3345 2.75024Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>                                </svg>                            </Button>
+                        </div>
+                      </td>
+                    </tr>
                   </tbody>
                 </Table>
-                <Row className="align-items-center">
-                  <Col md="6">
-                    <div
-                      className="dataTables_info"
-                      id="datatable_info"
-                      role="status"
-                      aria-live="polite"
-                    >
-                      Mostrar 1 a 10 de 57 entradas 
-                    </div>
-                  </Col>
-                  <Col md="6">
-                    <div
-                      className="dataTables_paginate paging_simple_numbers"
-                      id="datatable_paginate"
-                    >
-                      <ul className="pagination">
-                        <li
-                          className="paginate_button page-item previous disabled"
-                          id="datatable_previous"
-                        >
-                          <Link
-                            to="#"
-                            aria-controls="datatable"
-                            aria-disabled="true"
-                            data-dt-idx="previous"
-                            tabIndex="0"
-                            className="page-link"
-                          >
-                            Anterior
-                          </Link>
-                        </li>
-                        <li className="paginate_button page-item active">
-                          <Link
-                            to="#"
-                            aria-controls="datatable"
-                            aria-current="page"
-                            data-dt-idx="0"
-                            tabIndex="0"
-                            className="page-link"
-                          >
-                            1
-                          </Link>
-                        </li>
-                        <li className="paginate_button page-item ">
-                          <Link
-                            to="#"
-                            aria-controls="datatable"
-                            data-dt-idx="1"
-                            tabIndex="0"
-                            className="page-link"
-                          >
-                            2
-                          </Link>
-                        </li>
-                        <li className="paginate_button page-item ">
-                          <Link
-                            to="#"
-                            aria-controls="datatable"
-                            data-dt-idx="2"
-                            tabIndex="0"
-                            className="page-link"
-                          >
-                            3
-                          </Link>
-                        </li>
-                        <li className="paginate_button page-item ">
-                          <Link
-                            to="#"
-                            aria-controls="datatable"
-                            data-dt-idx="3"
-                            tabIndex="0"
-                            className="page-link"
-                          >
-                            4
-                          </Link>
-                        </li>
-                        <li className="paginate_button page-item ">
-                          <Link
-                            to="#"
-                            aria-controls="datatable"
-                            data-dt-idx="4"
-                            tabIndex="0"
-                            className="page-link"
-                          >
-                            5
-                          </Link>
-                        </li>
-                        <li className="paginate_button page-item ">
-                          <Link
-                            to="#"
-                            aria-controls="datatable"
-                            data-dt-idx="5"
-                            tabIndex="0"
-                            className="page-link"
-                          >
-                            6
-                          </Link>
-                        </li>
-                        <li
-                          className="paginate_button page-item next"
-                          id="datatable_next"
-                        >
-                          <Link
-                            to="#"
-                            aria-controls="datatable"
-                            data-dt-idx="next"
-                            tabIndex="0"
-                            className="page-link"
-                          >
-                            Próximo
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
-                  </Col>
-                </Row>
               </div>
             </Card.Body>
           </Card>
